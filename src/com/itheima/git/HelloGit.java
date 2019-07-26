063c0e632ed3ad006b1a80c8dd83a7bd2f6b487d
liwen <lw1016@163.com>
2019-07-26 18:42:22 +0800

    第一次发布项目git,编写HelloGit

---
+++ src/com/itheima/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.itheima.git;
+
+/**
+ * @author : Arvin
+ * @Project Name : git
+ * @Package Name : com.itheima.git
+ * @Creation Date: 2019-07-26 18:41
+ * @Description :
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
